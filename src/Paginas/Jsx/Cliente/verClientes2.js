41069c5c803ab03ca201b2e2c71d923a9889786f
rebeca <kattantoledo@gmail.com>
2021-05-05 19:19:37 -0600

    Distribucion de vendedores

--- src/Paginas/Jsx/Cliente/verClientes2.js
+++ src/Paginas/Jsx/Cliente/verClientes2.js
@@ -1,6 +1,3 @@
-/*ejemplo de class based component
-siempre debe heredar de la clase component */
-
 import React, {Component, useState} from 'react';
 import '../../../Paginas/menu.css';
 
@@ -8,28 +5,53 @@ class verClientes2 extends Component{
     constructor(props){
         super(props)
         
-        this.state ={ 
-            vendedores : [
+        const vendedoresPermanentes = [
             {nombre:'Luis Perez',edad:'25'},
             {nombre:'Carlos Robles',edad:'30'},
             {nombre:'Juan Chavez',edad:'34'},
             {nombre:'Maria Castro',edad:'24'}
-            ]
+        ];
+
+        this.state ={ 
+            datos : vendedoresPermanentes
         }
         
+        
         this.cambiarVendedor = this.cambiarVendedor.bind(this);
+        this.update =this.update.bind(this);
     }
 
-    cambiarVendedor(event){
-        this.setState({
-            vendedores : [
-                {nombre:'Stefany Guzman',edad:'42'},
-                {nombre:'Mario Reyes',edad:'33'},
-                {nombre:'Cristina Alfaro',edad:'24'},
-                {nombre:'Flor Cruz',edad:'31'}
-            ]
-        });
+    handleChange = (item) =>{
+        this.state({
+            vendedores:{
+                ...this.state.vendedores,
+                [item.target.name]: item.target.value
+            },
+        })
     }
+
+    cambiarVendedor(){
+
+        //vendedores sustitutos
+        const vendedoresSustitutos = [
+            {nombre:'Stefany Guzman',edad:'42'},
+            {nombre:'Mario Reyes',edad:'33'},
+            {nombre:'Cristina Alfaro',edad:'24'},
+            {nombre:'Flor Cruz',edad:'31'}
+        ]
+
+        this.setState({datos : vendedoresSustitutos});
+    }
+
+    update(dato,index) {
+       var contador = 0;
+       var lista = this.state.vendedores;
+       lista.map((registro=>{
+           if(dato.index ==registro.index){
+                lista[index].nombre=dato.nombre;
+           }
+       }))
+      }
 
     render(){
         return(
@@ -38,40 +60,41 @@ class verClientes2 extends Component{
                 <h2>Distribución de Vendedores <br/><br/></h2>
                 <form>
                     <div className="row justify-content-center">
-                        <div className="col-3">
+                        <div className="col-2">
                             {
-                            this.state.vendedores.map((item, index) => (
+                            this.state.datos.map((item, index) => (
                                 
                                 <ul key={index}>Nombre: <br /> {item.nombre}</ul>
                             ))
                             }
                         </div>
-                        <div className="col-3">
+                        <div className="col-2">
                             {
-                            this.state.vendedores.map((item, index) => (
+                            this.state.datos.map((item, index) => (
                                 
                                 <ul key={index}>Edad: <br /> {item.edad}</ul>
                             ))
                             }
                         </div>
-                        <div className="col-3">
+                        <div className="col-2">
                             {
-                            this.state.vendedores.map((item, index) => (
+                            this.state.datos.map((item, index) => (
                                 
                                 <ul key={index}>Cliente: <br />Cliente {index +1}</ul>
                             ))
                             }
                         </div>
-                        {/*<div className="col-3">
+                        <div className="col-4">
                             {
-                            this.state.vendedores.map((item, index) => (
+                            this.state.datos.map((item, index) => (
                                 
                                 <ul key={index}><small><b>Reasignar</b></small> <br />
-                                    <input name="nombreR" type="text" value="" placeholder="Escriba un nombre"></input>
+                                    <input name="nombre" type="text" placeholder="Escriba un nombre" ></input>
+                                    <button className="btn btn-outline-primary" onClick={()=>this.update(item,index)}>Actualizar</button>
                                 </ul>
                             ))
                             }
-                        </div>*/}
+                        </div>
                     </div>
                     <div className="row justify-content-center">
                         <div className="col-2">
